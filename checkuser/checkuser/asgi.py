9e0899b371b9f65bc99c57073834aee8861d7ed9
shrestha-milan <pasangsyangbo12@virinchicollege.edu.np>
2021-01-30 20:56:29 +0545

    new commit

--- checkuser/checkuser/asgi.py
+++ checkuser/checkuser/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for userapp project.
+ASGI config for checkuser project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'userapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'checkuser.settings')
 
 application = get_asgi_application()
--- checkuser/manage.py
+++ checkuser/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'userapp.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'checkuser.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
